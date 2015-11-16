ad090a7a1c50bf5ff115a63ca99ec4a0eaff8dfd
Roger Hoover <roger.hoover@gmail.com>
2015-11-16 12:44:54 -0800

    Bump to version 1.0-q3

--- camus-uber-jar/run.sh
+++ camus-uber-jar/run.sh
@@ -1,1 +1,1 @@
-java -Dlog4j.configuration=file:log4j.properties -cp target/confluent-camus-uber-jar-1.0-q2.jar com.linkedin.camus.etl.kafka.CamusJob -P camus.properties 
+java -Dlog4j.configuration=file:log4j.properties -cp target/confluent-camus-uber-jar-1.0-q3.jar com.linkedin.camus.etl.kafka.CamusJob -P camus.properties 
